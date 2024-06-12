4cef70e1ef77e89f92184d2dee261c3a0b9269ce
Anton Zhiganov <Antohajigan@gmail.com>
2024-06-12 14:20:30 +0200

    Completed task 2

---
+++ Lection_1/Task_2/main/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_main_FILE D:/Универ/NG_2024_Anton_Zhyhanov/Lection_1/Task_2/main/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/main.exe)
+set(__QT_DEPLOY_TARGET_main_RUNTIME_DLLS C:/NG/6.8.0/mingw_64/bin/Qt6Core.dll)
---
+++ Lection_1/Task_2/main/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter 2 numbers you want to add: " << endl;
+    int num1, num2;
+    cin >> num1;
+    cin >> num2;
+    cout << "Sum: " << num1 + num2 << endl;
+    return 0;
+}
