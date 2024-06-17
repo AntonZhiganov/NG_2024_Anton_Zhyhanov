24d08df60b182700d06591d78c96eb44fc9f8c9d
Anton Zhiganov <Antohajigan@gmail.com>
2024-06-17 20:14:33 +0200

    Removed unnecessary components

--- Lection_1/Task_1/main/build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_main_FILE D:/Универ/NG_2024_Anton_Zhyhanov/Lection_1/Task_1/main/build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/main.exe)
-set(__QT_DEPLOY_TARGET_main_RUNTIME_DLLS C:/NG/6.8.0/llvm-mingw_64/bin/Qt6Core.dll)
