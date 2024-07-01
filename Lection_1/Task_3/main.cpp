97786c4e426690d8eb9a8bddf928ec8fad95d6d6
Anton Zhiganov <Antohajigan@gmail.com>
2024-07-01 23:14:37 +0200

    Completed assignment 3 of lecture 1

---
+++ Lection_1/Task_3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int money = 0;
+    cout << "How much do you earn?" << endl;
+    cin >> money;
+
+    if (money < 1000){
+        cout << "Work harder" << endl;
+    }
+
+    if (money > 999){
+        if (money < 1000000){
+            cout << "You're doing a good job" << endl;
+        }
+    }
+
+    if (money > 999){
+        if (money > 999999){
+            cout << "Wow, you're a millionaire" << endl;
+        }
+    }
+
+    cout << "But you're great" << endl;
+    return 0;
+}
