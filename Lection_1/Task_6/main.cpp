53f42ab4383a2ae2a6f11ab47abfdefd8e28a388
Anton Zhiganov <Antohajigan@gmail.com>
2024-07-10 08:56:15 +0200

    Fixed errors

--- Lection_1/Task_6/main.cpp
+++ Lection_1/Task_6/main.cpp
@@ -3,37 +3,26 @@
 using namespace std;
 
 void DrawTree(int size) {
-
     for (int i = 1; i <= size; ++i) {
-
         for (int j = i; j < size; ++j) {
-
             cout << " ";
-
         }
         for (int j = 1; j <= (2 * i - 1); ++j) {
-
             cout << "*";
-
         }
         cout << endl;
     }
 
     for (int i = 0; i < size - 1; ++i) {
-
         cout << " ";
-
     }
-
     cout << "*" << endl;
 }
 
 int main() {
-
     int size;
     cout << "Enter size: ";
     cin >> size;
     DrawTree(size);
     return 0;
-
 }
