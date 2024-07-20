832287bb22993d50c5d08e472c1094219203af67
Anton Zhiganov <Antohajigan@gmail.com>
2024-07-20 13:24:46 +0200

    Now we check if the stalker is inside the circle

--- Lection_1/Task_7/main.cpp
+++ Lection_1/Task_7/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     float N, x, y;
-    cout << "Enter field radius(N): " << endl;
+    cout << "Enter field radius (N): " << endl;
     cin >> N;
     cout << "Enter x coordinate (x): " << endl;
     cin >> x;
@@ -14,7 +14,12 @@ int main()
     cin >> y;
 
     float distance = sqrt(x*x + y*y);
-    cout << "Detector blinking frequency: " << distance << endl;
+
+    if (distance <= N) {
+        cout << "Detector blinking frequency: " << distance << endl;
+    } else {
+        cout << "Point is outside the field radius." << endl;
+    }
 
     return 0;
 }
